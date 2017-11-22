ece98acee56732b501f5746cb0be62aac3a43247
Nils Brinkmann <nils.brinkmann@ubimax.com>
2017-11-22 15:46:19 +0100

    Initial commit, stripped HoloToolkit etc out of it to make it smaller in size

---
+++ Assets/SetPosToCam.cs
@@ -0,0 +1,38 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class SetPosToCam : MonoBehaviour, IInputClickHandler {
+
+    private bool IsCurrentlyDragging = true;
+    private Vector3 startRot = Vector3.zero;
+
+    void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        IsCurrentlyDragging = !IsCurrentlyDragging;
+        if(IsCurrentlyDragging)
+        {
+            transform.SetParent(Camera.main.transform, true);
+        }
+        else
+        {
+            transform.parent = null;
+        }
+    }
+    
+	void Update ()
+    {
+        var pos = transform.position;
+        pos.y = 0.0f;
+        transform.position = pos;
+
+        var euler = transform.eulerAngles;
+        euler.x = 0.0f;
+        euler.z = 0.0f;
+        transform.eulerAngles = euler;
+    }
+}
